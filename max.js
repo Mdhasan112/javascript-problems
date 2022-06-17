08cd9525827464ae1e31ff84faee9dc783f5a212
hasan <mdhasanhk.98@gmail.com>
2022-06-17 06:27:20 -0400

    max

---
+++ max.js
@@ -0,0 +1,20 @@
+var business = 300;
+var minister = 400;
+var sochib = 350;
+
+if(business > minister) {
+    if(business > sochib) {
+        console.log("business is bigge")
+    }
+    else{
+        console.log("sochib is bigger")
+    }
+}
+else{
+    if(minister > sochib) {
+        console.log("minister is bigger")
+    }
+    else{
+        console.log("sochib is bigger")
+    }
+}
