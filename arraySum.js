710cb6e386ecf2931729a8f37645dabef8830d27
hasan <mdhasanhk.98@gmail.com>
2022-06-18 15:52:02 -0400

    arraySum

--- arraySum.js
+++ arraySum.js
@@ -9,3 +9,25 @@ for(var i = 0; i < num.length; i++) {
     
 }
 console.log(sum)
+
+
+
+function arraySum(num) {
+    var sum = 0;
+
+for(var i = 0; i < num.length; i++) {
+    var element = num[i];
+
+    sum = sum + element;
+
+    
+}
+    return sum;
+
+}
+var num = [10, 30, 50, 5, 59, 60];
+var result = arraySum(num);
+console.log(result);
+
+var total = arraySum([40, 70, 67, 59,80]);
+console.log(total);
