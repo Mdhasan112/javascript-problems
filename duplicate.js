aa74e405f402a8acca851159a42cc3befe3dfe34
hasan <mdhasanhk.98@gmail.com>
2022-06-19 00:39:18 -0400

    duplicate

---
+++ duplicate.js
@@ -0,0 +1,12 @@
+var Name = [50, 7, 7, 8, 9, 9, 5, 50]
+var uniqueName = [];
+
+for(i = 0; i < Name.length; i++) {
+    var element = Name[i];
+    var index = uniqueName.indexOf(element);
+
+    if(index == -1) {
+        uniqueName.push(element);
+    }
+}
+console.log(uniqueName)
